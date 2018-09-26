94b708e78b05513b7d9824dbb930403707b08478
trinhanh268 <43316046+trinhanh268@users.noreply.github.com>
2018-09-26 12:01:08 +0700

    Update Task1.java

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -11,7 +11,7 @@ public class Task1 {
 
     public static int fibonacci(int n) {
         // TODO: Tìm số fibonacci ở vị trí n
-        if( n == 1 || n == 2) return 1;
-        if(n > 2) return fibonacci( n - 1) + fibonacci( n - 2 );
+        if ( n == 0) return 0;
+        if ( n == 1) return 1;
+        if ( n > 1) return fibonacci( n - 1 ) + fibonacci( n - 2);
     }
-}
