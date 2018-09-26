06e81c5b342d0020ff265b46f8a0fa7a5cfa66e2
trinhanh268 <43316046+trinhanh268@users.noreply.github.com>
2018-09-26 11:53:59 +0700

    Update Task1.java

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -4,11 +4,14 @@ public class Task1 {
 
     public static int gcd(int a, int b) {
         // TODO: Tính ước chung lớn nhất của 2 số a, b
-        return 0;
+        if( a > b) return gcd( a - b, b);
+        else if ( a < b) return gcd( a, b - a);
+        else return a;
     }
 
     public static int fibonacci(int n) {
         // TODO: Tìm số fibonacci ở vị trí n
-        return 0;
+        if( n == 1 || n == 2) return 1;
+        if(n > 2) return fibonacci( n - 1) + fibonacci( n - 2 );
     }
 }
