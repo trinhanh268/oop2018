60ca7e0700806d71b09947570516c72be2025237
trinhanh268 <43316046+trinhanh268@users.noreply.github.com>
2018-09-26 17:19:03 +0700

    Update Task3.java

--- src/week2/task3/Task3.java
+++ src/week2/task3/Task3.java
@@ -13,12 +13,38 @@ public class Task3 {
     }
     public void myjob(){
       System.out.print(this.job);
-    }
+    }}
   public animal(){
     public String kind;
     public int age;
     public int amount;
-    public void kind
+    public void kind(){
+      System.out.println(this.kind);
+      System.out.println(this.amount + " left ");
+    }
+    public void check(){
+     if (age < 10) System.out.println("Still small. Need help");
+     else System.out.println("This is growning");
+    }
+    public void sell(){
+      if(amount > 10000) System.out.println("Cheap");
+      else System.out.println("Expensive");
+    }}
+    public car(){
+      public String color;
+      public int price;
+      public int speed;
+      public void color(){
+        System.out.println(this.color);
+        }
+      public void price(){
+        System.out.println(this.price);
+      }
+      public void speed(){
+        System.out.println(this.kind);
+      }
+    }
 }
+    
 
 //TODO: khai báo 3 class tương ứng với 3 đối tượng thực tế ở dưới
