4883b653b3e032fa77e47ce79af2b383295e919f
trinhanh268 <43316046+trinhanh268@users.noreply.github.com>
2018-09-26 16:23:10 +0700

    Update Fraction.java

--- src/week2/task2/Fraction.java
+++ src/week2/task2/Fraction.java
@@ -3,28 +3,41 @@ package week2.task2;
 public class Fraction {
 
     // TODO: khai báo các thuộc tính
-
+    public int numerator;
+    public int denominator;
     public Fraction(int numerator, int denominator) {
         // TODO: khởi tạo giá trị cho các thuộc tính numberator (tử số), denominator (mẫu số)
-    }
+        this.numerator = numerator;
+        this.denominator = denominator;
+     }
 
     public Fraction add(Fraction other) {
         // TODO: Phương thức cộng hai phân số (this và other), trả về đối tượng Fraction mới
-        return null;
+        Fraction result;
+        result.numerator = this.numerator * orther.denominator + this.denominator * orther.numerator;
+        result.denominator = this.denominator * orther.denominator;
+        return result;
     }
 
     public Fraction subtract(Fraction other) {
         // TODO: Phương thức trừ hai phân số (this và other), trả về đối tượng Fraction mới
-        return null;
+        Fraction result;
+        result.numerator = this.numerator * orther.denominator - orther.numerator * this.denominator;
+        result.denominator = this.denominator * orther.denominator;
+        return result;
     }
 
     public Fraction multiply(Fraction other) {
         // TODO: Phương thức nhân hai phân số (this và other), trả về đối tượng Fraction mới
-        return null;
+        result.numerator = this.numerator * orther.numerator;
+        result.denominator = this.denominator * orther.denominator;
+        return result;
     }
 
     public Fraction divide(Fraction other) {
         // TODO: Phương thức chia hai phân số (this và other), trả về đối tượng Fraction mới
-        return null;
+        result.numerator = this.numerator * orther.denominator;
+        result.denominator = this.denominator * orther.numerator;
+        return result;
     }
 }
