e0819285043c8dc57fe13e15e71f979afaad5486
trinhanh268 <43316046+trinhanh268@users.noreply.github.com>
2018-09-26 17:01:52 +0700

    Update Task3.java

--- src/week2/task3/Task3.java
+++ src/week2/task3/Task3.java
@@ -1,6 +1,24 @@
 package week2.task3;
 
 public class Task3 {
+  public person(){
+    public String name;
+    public int age;
+    public String job;
+    public void Greeting(){
+      System.out.print("Hello. My name is " + this.name);
+    }
+    public void Introduce(){
+      System.out.print("I am " + age + " years old");
+    }
+    public void myjob(){
+      System.out.print(this.job);
+    }
+  public animal(){
+    public String kind;
+    public int age;
+    public int amount;
+    public void kind
 }
 
 //TODO: khai báo 3 class tương ứng với 3 đối tượng thực tế ở dưới
