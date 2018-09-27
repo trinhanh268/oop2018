73c4bbe60900f40686906a6391f893f559a4ee93
trinhanh268 <43316046+trinhanh268@users.noreply.github.com>
2018-09-27 10:32:07 +0700

    Update Fraction.java

--- src/week2/task2/Fraction.java
+++ src/week2/task2/Fraction.java
@@ -1,5 +1,7 @@
 package week2.task2;
 
+import week2.task1.Task1;
+
 public class Fraction {
 
     // TODO: khai báo các thuộc tính
@@ -16,7 +18,7 @@ public class Fraction {
         Fraction result = new Fraction(0,0);
         result.numerator = this.numerator * orther.denominator + this.denominator * orther.numerator;
         result.denominator = this.denominator * orther.denominator;
-        return result;
+        return result.minimal();
     }
 
     public Fraction subtract(Fraction orther) {
@@ -24,14 +26,22 @@ public class Fraction {
         Fraction result = new Fraction(0,0);
         result.numerator = this.numerator * orther.denominator - orther.numerator * this.denominator;
         result.denominator = this.denominator * orther.denominator;
-        return result;
+        return result.minimal();
     }
 
-    public Fraction multiply(Fraction orther) {
+    public Fraction multiply(Fraction other) {
         // TODO: Phương thức nhân hai phân số (this và other), trả về đối tượng Fraction mới
         Fraction result = new Fraction(0,0);
-        result.numerator = this.numerator * orther.numerator;
-        result.denominator = this.denominator * orther.denominator;
+        
+        result.numerator = this.numerator*other.numerator;
+        result.denominator = this.denominator*other.denominator;
+        return result.minimal();
+    }
+    public Fraction minimal(){
+        Fraction result = new Fraction(0,0);
+        int ucln = Task1.gcd(this.numerator, this.denominator);
+        result.numerator = this.numerator/ucln;
+        result.denominator = this.denominator /ucln;
         return result;
     }
 
@@ -40,16 +50,17 @@ public class Fraction {
         Fraction result = new Fraction(0,0);
         result.numerator = this.numerator * orther.denominator;
         result.denominator = this.denominator * orther.numerator;
-        return result;
+        return result.minimal();
     }
-    public boolean equals(Fraction orther)
+    public boolean equals(Object obj)
     {
-        if ( this.numerator / this.denominator == orther.numerator / orther.denominator) return true;
+        Fraction other= (Fraction) obj;
+        if ( this.numerator / this.denominator == other.numerator / other.denominator) return true;
         else return false;
     }
     public static void main(String[] args){
-        Fraction num1 = new Fraction(1,4);
-        Fraction num2 = new Fraction(2,8);
+        Fraction num1 = new Fraction(-1,4);
+        Fraction num2 = new Fraction(-2,8);
         Fraction result ;
 
         result = num1.add(num2);
@@ -58,16 +69,16 @@ public class Fraction {
         result = num1.subtract(num2);
         System.out.println(num1.numerator + "/" + num1.denominator + " - " + num2.numerator + "/" + num2.denominator + " = " + result.numerator + "/" + result.denominator);
 
-        result = num1.multiply(num2);
+        result=num1.multiply(num2);
         System.out.println(num1.numerator + "/" + num1.denominator + " * " + num2.numerator + "/" + num2.denominator + " = " + result.numerator + "/" + result.denominator);
 
         result = num1.divide(num2);
         System.out.println(num1.numerator + "/" + num1.denominator + " : " + num2.numerator + "/" + num2.denominator + " = " + result.numerator + "/" + result.denominator);
 
-        if(num1.equals(num2)){
-            System.out.println("Lon hon");
+        if(num1.equals(num2) == true){
+            System.out.println("Bang nhau");
         }
-        else System.out.println("Nho hon");
+        else System.out.println("Khac nhau");
     }
 }
 
