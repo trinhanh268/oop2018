6dce0c51d307e37539426aaabeff8f14e1f43607
trinhanh268 <43316046+trinhanh268@users.noreply.github.com>
2018-10-03 21:38:57 +0700

    Update Week3.java

--- src/week3/Week3.java
+++ src/week3/Week3.java
@@ -28,6 +28,13 @@ public class Week3 {
      */
     public static String calculateBMI(double weight, double height) {
         // TODO: Viết chương trình tính chỉ số BMI và in ra kết quả đánh giá
-        return null;
-    }
+        double BMI;
+        String appearence;
+        BMI = weight / ( height * height );
+        if ( BMI < 18.5 ) appearence = "Thieu can";
+        else if( BMI > 18.5 && BMI < 25) appearence = "Binh thuong";
+        else if( BMI >= 23 && BMI < 25) appearence = "Thua can";
+        else if( BMI >= 25 ) appearence = "Beo phi";
+        return appearence;
+            }
 }
