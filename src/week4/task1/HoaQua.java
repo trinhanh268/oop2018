afd120f64680f8062804cabe564bc2d367897402
trinhanh268 <43316046+trinhanh268@users.noreply.github.com>
2018-10-10 22:54:07 +0700

    Update HoaQua.java

--- src/week4/task1/HoaQua.java
+++ src/week4/task1/HoaQua.java
@@ -3,4 +3,43 @@ package week4.task1;
 /**
  * @author DucAnh
  */
- public class HoaQua
+ public class HoaQua{
+  private int gia;
+  private String Season;
+  /**
+    *@param gia gia ban trai cay
+    */
+  public void Setgia(int gia){
+   this.gia = gia;
+  }
+ /**
+ * @return gia gia ban
+ */
+  public int Getgia(){
+   return gia;
+  }
+ /**
+   *@param Season hoa qua mua gi
+   */ 
+ public void SetSeason(String Season){
+  this.Season = Season;
+ }
+ /**
+  * @return Season hoa qua mua gi
+  */
+  public String GetSeason(){
+   return Season;
+  }
+  /**
+   * constructor
+   * @param gia gia ban trai cay
+   * @param Season hoa qua mua gi
+   */
+  public HoaQua( int gia, String Season){
+   this.gia = gia;
+   this.Season = Season;
+  }
+  /**
+   * 
+  
+  
