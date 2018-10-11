ecbf0d391b35785fb0919905ce89bbd0f824f44e
trinhanh268 <43316046+trinhanh268@users.noreply.github.com>
2018-10-11 09:23:07 +0700

    Create Tao.java

---
+++ src/week4/task1/Tao.java
@@ -0,0 +1,21 @@
+package week4.task1;
+public class Tao extends HoaQua{
+  public Tao(int gia, String Season)
+  {
+    super(gia,Season);
+  }
+  /**
+   * xem gia ban
+   */
+  public void xemgia()
+  {
+   System.out.print("Gia ban: " + gia);
+  }
+  /**
+   * hoa qua mua gi
+   */
+  public void muagi()
+  {
+   System.out.printf("Hoa qua mua: " + Season);
+  }
+  }
