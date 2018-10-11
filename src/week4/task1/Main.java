f36f97a0fd5e8f75bef7fabcec830b994f2f61d1
trinhanh268 <43316046+trinhanh268@users.noreply.github.com>
2018-10-11 09:29:52 +0700

    Create Main.java

---
+++ src/week4/task1/Main.java
@@ -0,0 +1,17 @@
+package week4.task1;
+
+public class Main{
+  public static void main(String[] args){
+    Cam cam = new Cam( 20000, "Mua dong");
+    CamThanhPhong CTP = new CamThanhPhong( 30000, "Mua dong");
+    CamSanh CS = new CamSanh(25000, " Mua dong");
+    Tao tao = new Tao(15000, " Mua he");
+    cam.xemgia();
+    cam.muagi();
+    CTP.xemgia();
+    CTP.muagi();
+    CS.xemgia();
+    CS.muagi();
+    tao.xemgia();
+    tao.muagi();
+    }
