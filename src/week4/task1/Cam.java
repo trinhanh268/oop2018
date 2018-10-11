35e07769601e20a26fe7849526aea5a36b62edde
trinhanh268 <43316046+trinhanh268@users.noreply.github.com>
2018-10-11 09:14:34 +0700

    Update Cam.java

--- src/week4/task1/Cam.java
+++ src/week4/task1/Cam.java
@@ -1,4 +1,12 @@
 package week4.task1
 
 public class Cam extends HoaQua{
-  Cam(
+  public Cam(int gia, String Season)
+  {
+   super(gia, Season);
+  }
+  public void checkgia(){
+    System.out.printf("Gia 1 can la: " + gia);
+    }
+   }
+    
