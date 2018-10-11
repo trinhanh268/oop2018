1bdfe4ce0cc47032736ddea9f912c916e32c8ac4
CCNE <CCNE@10.0.2.15>
2018-10-11 09:58:58 +0700

    ducanh

---
+++ src/week4/task2/Rectangle.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week4.task2;
+
+/**
+ *
+ * @author CCNE
+ */
+public class Rectangle extends Shape{
+    private double length,width;
+    public Rectangle(){
+        }
+    public Rectangle(double width, double length)
+    {
+        this.width = width;
+        this.length = length;
+    }
+    
+}
